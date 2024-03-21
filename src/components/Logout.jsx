a944e1ca1849dd1ed57648ae70ddecba00061815
Aakash <aakashkumar2908@gmail.com>
2024-03-22 00:16:30 +0530

    seventeenth commit

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,32 +1,59 @@
-// import {useEffect} from "react";
-import {Navigate, useNavigate} from "react-router-dom";
+// // import {useEffect} from "react";
+// import {Navigate, useNavigate} from "react-router-dom";
+// import { useAuth } from "./verify/Auth";
+
+
+// const Logout = () => {
+
+//   const { isLoggedIn, LogoutUser } = useAuth();
+//   // const navigate = useNavigate();
+
+//   // If the user is not logged in, redirect to the login page
+//   if (!isLoggedIn) {
+//       return <Navigate to="/login" />;
+//   }
+
+//   // useEffect(()=>{
+//   //   LogoutUser();
+//   // },[LogoutUser]);
+
+//   const handleLogout=async()=>{
+//     await LogoutUser();
+//     // window.location.reload();
+//   }
+
+//   // return <Navigate to="/login" />;
+//   return(
+//     <div>
+//       <button onClick={handleLogout}>Logout</button>
+//     </div>
+//   )
+// }
+// export default Logout;
+
+
+
+import React from "react";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "./verify/Auth";
 
-
 const Logout = () => {
-
   const { isLoggedIn, LogoutUser } = useAuth();
-  // const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await LogoutUser();
+  };
 
   // If the user is not logged in, redirect to the login page
   if (!isLoggedIn) {
-      return <Navigate to="/login" />;
+    return <Navigate to="/login" />;
   }
 
-  // useEffect(()=>{
-  //   LogoutUser();
-  // },[LogoutUser]);
-
-  const handleLogout=async()=>{
-    await LogoutUser();
-    // window.location.reload();
-  }
-
-  // return <Navigate to="/login" />;
-  return(
+  return (
     <div>
       <button onClick={handleLogout}>Logout</button>
     </div>
-  )
-}
+  );
+};
+
 export default Logout;
