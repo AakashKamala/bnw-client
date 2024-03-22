6c747fa701edd075cec2b42f6ac9a6e250e8ca63
Aakash <aakashkumar2908@gmail.com>
2024-03-22 09:46:20 +0530

    user update

--- src/components/verify/Auth.jsx
+++ src/components/verify/Auth.jsx
@@ -84,79 +84,140 @@
 
 
 
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { baseURL } from "../../../url";
+// import React, { createContext, useContext, useEffect, useState } from "react";
+// import { baseURL } from "../../../url";
 
-export const AuthContext = createContext();
+// export const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem("token"));
-  const [user, setUser] = useState("");
+// export const AuthProvider = ({ children }) => {
+//   const [token, setToken] = useState(localStorage.getItem("token"));
+//   const [user, setUser] = useState("");
 
-  const storeTokenInLS = (serverToken) => {
-    setToken(serverToken);
-    return localStorage.setItem("token", serverToken);
-  };
+//   const storeTokenInLS = (serverToken) => {
+//     setToken(serverToken);
+//     return localStorage.setItem("token", serverToken);
+//   };
 
-  let isLoggedIn = !!token;
+//   let isLoggedIn = !!token;
 
-  const LogoutUser = () => {
-    setToken("");
-    setUser(""); // Reset the user state when logging out
-    return localStorage.removeItem("token");
-  };
+//   const LogoutUser = () => {
+//     setToken("");
+//     setUser(""); // Reset the user state when logging out
+//     return localStorage.removeItem("token");
+//   };
 
-  // Inside the AuthProvider component
-// const [isLoggedIn, setIsLoggedIn] = useState(!!token);
+//   // Inside the AuthProvider component
+// // const [isLoggedIn, setIsLoggedIn] = useState(!!token);
 
-// const LogoutUser = () => {
-//   setToken("");
-//   setUser(""); // Reset the user state when logging out
-//   setIsLoggedIn(false); // Update the isLoggedIn state
-//   return localStorage.removeItem("token");
+// // const LogoutUser = () => {
+// //   setToken("");
+// //   setUser(""); // Reset the user state when logging out
+// //   setIsLoggedIn(false); // Update the isLoggedIn state
+// //   return localStorage.removeItem("token");
+// // };
+
+//   const userAuthentication = async () => {
+//     try {
+//       const response = await fetch(`${baseURL}/api/auth/user`, {
+//         method: "GET",
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       });
+//       if (response.ok) {
+//         const data = await response.json();
+//         setUser((prevUser) => {
+//           if (prevUser !== data.userData) {
+//             return data.userData;
+//           }
+//           return prevUser;
+//         });
+//       }
+//     } catch (error) {
+//       console.error("Error fetching user data");
+//     }
+//   };
+
+//   const fetchUserData = async () => {
+//     await userAuthentication();
+//   };
+
+//   useEffect(() => {
+//     fetchUserData();
+//   }, []);
+
+//   return (
+//     <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser, user, fetchUserData }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
 // };
 
-  const userAuthentication = async () => {
-    try {
-      const response = await fetch(`${baseURL}/api/auth/user`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (response.ok) {
-        const data = await response.json();
-        setUser((prevUser) => {
-          if (prevUser !== data.userData) {
-            return data.userData;
-          }
-          return prevUser;
-        });
-      }
-    } catch (error) {
-      console.error("Error fetching user data");
-    }
-  };
+// export const useAuth = () => {
+//   const authContextValue = useContext(AuthContext);
+//   if (!authContextValue) {
+//     throw new Error("useAuth used outside of the Provider");
+//   }
+//   return authContextValue;
+// };
 
-  const fetchUserData = async () => {
-    await userAuthentication();
-  };
 
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import BuyForm from './components/BuyForm';
+import SignupForm from './components/SignupForm';
+import Login from './components/Login';
+import Email from './components/verify/Email';
+import Mobile from './components/verify/Mobile';
+import CustomerDetails from './components/CustomerDetails';
+import CustomerEmail from './components/verify/CustomerEmail';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+
+import { useAuth } from './components/verify/Auth';
+import EditCustomerDetails from './components/EditCustomerDetails';
+import CustomerLogin from './components/CustomerLogin';
+import Footer from './components/Footer';
+
+function App() {
+  const { isLoggedIn } = useAuth();
+  const [isAuthenticated, setIsAuthenticated] = useState(isLoggedIn);
+
+  // Subscribe to changes in isLoggedIn
   useEffect(() => {
-    fetchUserData();
-  }, []);
+    setIsAuthenticated(isLoggedIn);
+  }, [isLoggedIn]);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser, user, fetchUserData }}>
-      {children}
-    </AuthContext.Provider>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:_id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/buy" element={<BuyForm />} />
+          <Route path="/login" element={<Login key={isAuthenticated ? 'loggedIn' : 'loggedOut'} />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/email" element={<Email />} />
+          <Route path="/mobile" element={<Mobile />} />
+          <Route path="/customer-details" element={<CustomerDetails />} />
+          <Route path="/customer-email" element={<CustomerEmail />} />
+          <Route path="/customer-login" element={<CustomerLogin />} />
+          <Route path="/edit-customer-details" element={<EditCustomerDetails />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-};
+}
 
-export const useAuth = () => {
-  const authContextValue = useContext(AuthContext);
-  if (!authContextValue) {
-    throw new Error("useAuth used outside of the Provider");
-  }
-  return authContextValue;
-};
+export default App;
